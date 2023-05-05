27e393a1667f1f64d0b3892c62c53699842f64e8
moshe <lerner1737@gmail.com>
2023-05-05 18:38:56 +0300

    got shadcn ui running

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Home } from './components/home'
 import { Popular } from './components/popular'
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./components/ui/select"
+
 
 function App() {
   
@@ -11,6 +19,16 @@ function App() {
     <BrowserRouter>
       <div className="h-10vh w-full bg-opacity-95 bg-gray-800 flex items-center">
         <div className='logo'></div>
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Theme" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="light">Light</SelectItem>
+            <SelectItem value="dark">Dark</SelectItem>
+            <SelectItem value="system">System</SelectItem>
+          </SelectContent>
+        </Select>
       </div>
       <Routes>
         <Route path="/" element={<Home/>}/>
