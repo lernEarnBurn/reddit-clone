13889956a7b3621165e8f070b689195248f48655
lernEarnBurn <lerners124@gmail.com>
2023-05-10 23:43:59 +0300

    added sign up; not working yet

--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -1,40 +1,71 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Button } from './ui/button';
 import { Avatar, AvatarFallback, AvatarImage  } from '@radix-ui/react-avatar';
 import { Input } from './ui/input';
 
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import type { RefObject } from 'react';
 
 import {
     getAuth,
-    onAuthStateChanged,
+    //onAuthStateChanged,
     GoogleAuthProvider,
     signInWithPopup,
-    signOut,
+    //signOut,
+    createUserWithEmailAndPassword,
+    //signInWithEmailAndPassword
   } from 'firebase/auth';
   
 interface LoginProps {
     setShowLogIn: React.Dispatch<React.SetStateAction<boolean>>;
+    setLoggedIn:  React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+//api key wrong and returning 404 on sign up
 
-export function Login(props: LoginProps){
+export function Loginform(props: LoginProps){
 
-    const [signUp, setSignUp] = useState(false)
+    const emailRef: RefObject<HTMLInputElement> = useRef(null)
+    const passwordRef: RefObject<HTMLInputElement> = useRef(null)
+
+    const [onSignUp, setSignUp] = useState(false)
 
     async function logInGoogle() {
         /*may just keep as show and not waste one of my google apps on it */
         const provider = new GoogleAuthProvider();
         await signInWithPopup(getAuth(), provider);
-      }
+    }
+
+    async function signUp(){
+      
+      const email = emailRef.current?.value
+      const password = passwordRef.current?.value
+      const auth = getAuth()
+
+      if (email && password) {
+        try {
+          const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+          const user = userCredential.user
+          console.log(user)
+          props.setLoggedIn(true)
+          props.setShowLogIn(false)
+        } catch (error: unknown) {
+          if (error instanceof Error) {
+            const errorMessage = error.message
+            console.log(errorMessage)
+          }
+        }
+      }        
+    }
 
     return (
         <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gray-500 bg-opacity-50">
               <div className="bg-white rounded-lg p-6 h-[65vh] w-[25vw] flex-col items-center">
                 <div className='flex justify-between'>
-                    {signUp ? (
-                        <h2 className="text-2xl font-medium mb-4">Sign Up</h2>
+                    {onSignUp ? (
+                        <h2 className="text-3xl font-medium mb-4">Sign Up</h2>
                     ) : (
-                        <h2 className="text-2xl font-medium mb-4">Login</h2>
+                        <h2 className="text-3xl font-medium mb-4">Login</h2>
                     )}
                   <button className='mb-12 mr-2 p-1' onClick={() => {props.setShowLogIn(false)}}>X</button>
                 </div>
@@ -45,11 +76,11 @@ export function Login(props: LoginProps){
                   </Avatar>
                   <p className='text-lg mr-10'>Sign-in with Google</p>
                 </button>
-                <Input className=' mb-4' placeholder='Email'/>
-                <Input placeholder='Password'/>
-                {signUp ? (
+                <Input ref={emailRef} type="email" className=' mb-4' placeholder='Email'/>
+                <Input ref={passwordRef} type="password" placeholder='Password'/>
+                {onSignUp ? (
                     <> 
-                        <Button size={"lg"} className='mt-4 w-full'>Submit</Button>  
+                        <Button onClick={signUp} size={"lg"} className='mt-4 w-full'>Submit</Button>  
                         <div className='flex mt-6 gap-2 items-center'>
                           <p>Already Have An Account?</p>
                           <a onClick={() => {setSignUp(false)}} className='text-sm cursor-pointer text-blue-500 underline'>Log In</a>
