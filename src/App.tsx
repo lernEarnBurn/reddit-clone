47e1aa8aa02bb8449013392f7156b9d5368cca05
lernEarnBurn <lerners124@gmail.com>
2023-05-17 13:27:18 +0300

    limited rerendering bug

--- src/App.tsx
+++ src/App.tsx
@@ -21,15 +21,54 @@ function App() {
   const [showLogIn, setShowLogIn] = useState(false)
 
   const [user, setUser] = useState("")
+  const [loading, setLoading] = useState(true)
   
+  /*there is a visual bug that I need to take care of */
   useEffect(() => {
-    const savedUser = localStorage.getItem('user');
-    if(savedUser){
-      setLoggedIn(true)
-      setShowLogIn(false)
-      setUser(savedUser)
+    const img = new Image();
+    img.src = '/images/stockAvatar.png';
+    img.onload = () => {
+      const savedUser = localStorage.getItem('user');
+      if(savedUser){
+        setLoggedIn(true)
+        setShowLogIn(false)
+        setUser(savedUser)
+        setLoading(false)
+      }
+    };
+
+    // Clean up by removing the onload event handler
+    return () => {
+      img.onload = null;
+    };
+   
+  }, [])
+
+  const selectRef = useRef<HTMLSelectElement>(null);
+  const [currentSelectValue, setSelectValue] = useState('/')
+  
+  function changeRoute() {
+    const selectedValue = selectRef.current?.value;
+    if (selectedValue) {
+      localStorage.setItem("selectedValue", selectedValue);
+      window.location.href = selectedValue;
+      setSelectValue(selectedValue);
     }
-  },[])
+  }
+
+  useEffect(() => {
+    const savedValue = localStorage.getItem("selectedValue");
+    if (savedValue && savedValue !== currentSelectValue) {
+      setSelectValue(savedValue);
+      if(selectRef.current){
+        selectRef.current.value = savedValue;
+      }
+      
+    }
+  }, [currentSelectValue]);
+
+  
+
 
   function logOut(){
     setUser("")
@@ -43,30 +82,6 @@ function App() {
     setShowLogIn(true)
   }
 
-  const selectRef = useRef<HTMLSelectElement>(null);
-  const [currentSelectValue, setSelectValue] = useState('/')
-  
-  function changeRoute() {
-    const selectedValue = selectRef.current?.value;
-    if (selectedValue) {
-      localStorage.setItem("selectedValue", selectedValue);
-      window.location.href = selectedValue;
-      setSelectValue(selectedValue);
-    }
-  }
-  
-  useEffect(() => {
-    const savedValue = localStorage.getItem("selectedValue");
-    if (savedValue && savedValue !== currentSelectValue) {
-      setSelectValue(savedValue);
-      if(selectRef.current){
-        selectRef.current.value = savedValue;
-      }
-      
-    }
-  }, [currentSelectValue]);
-
- 
 
   return (
     <BrowserRouter>
@@ -83,7 +98,7 @@ function App() {
             <option value="/create-subgeddit">Create Subgeddit</option>
           </select>
         <SearchBar className='w-[32vw] ml-10 primary-foreground font-medium border-gray-900 focus:border-gray-50' placeholder='Search Geddit' />
-        <div className='flex gap-5 ml-20'>
+        <div className='flex gap-5 ml-14'>
           <div style={{backgroundImage: "url('/images/notification.svg')"}} className='icon'></div>
           <div style={{backgroundImage: "url('/images/message.svg')"}} className='icon'></div>
           <div style={{backgroundImage: "url('/images/advertise.svg')"}} className='icon'></div>
@@ -94,35 +109,46 @@ function App() {
           /*This needs to be packaged as its own component to simplify things so i'm not in a conditionally rendered disaster */
             <Loginform setShowLogIn={setShowLogIn} setLoggedIn={setLoggedIn} setUser={setUser}/>
          )}
-      {loggedIn ? (
-          <div className='flex items-start justify-center mr-8 primary-foreground bg-gray-900 rounded-md'>
+      {!loggedIn && (localStorage.getItem('user') === null)  ? (
+          <>
+          <Button size={"lg"} variant="default" onClick={initiateLogIn}>Log In</Button>
+          <Avatar className='w-[4.5vw] h-[6vh] bg-contain ml-8'>
+            <AvatarImage src='/images/stockAvatar.png' />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </>
+        ) : loading ? (
+          <div className='flex items-start justify-center  primary-foreground bg-gray-900 rounded-md absolute right-4'>
             <Avatar className='w-[4.5vw] h-[6vh] bg-contain '>
-              <AvatarImage src="/images/stockAvatar.png" />
-              <AvatarFallback>CN</AvatarFallback>
+              <AvatarImage src='/images/stockAvatar.png'/>
             </Avatar>
             <div className="flex-col">
               <p className='mt-.5 mr-4'>{user}</p>
-              <p onClick={logOut} className='text-xs cursor-pointer text-gray-100 underline'>Log Out</p>
             </div>
           </div>
+          
+          
         ) : (
-          <>
-            <Button size={"lg"} variant="default" onClick={initiateLogIn}>Log In</Button>
-            <Avatar className='w-[4.5vw] h-[6vh] bg-contain ml-8'>
-              <AvatarImage src="/images/stockAvatar.png" />
-              <AvatarFallback>CN</AvatarFallback>
+            <div className='flex items-start justify-center  primary-foreground bg-gray-900 rounded-md absolute right-4'>
+            <Avatar className='w-[4.5vw] h-[6vh] bg-contain '>
+              <AvatarImage src='/images/stockAvatar.png' />
             </Avatar>
-          </>
+            <div className="flex-col">
+              <p className='mt-.5 mr-4'>{user}</p>
+              <p onClick={logOut} className='text-xs cursor-pointer text-gray-100 underline absolute right-30 top-5.5'>Log Out</p>
+            </div>
+          </div>
         )}
       </div>
-      <Routes>
-        
+      <Routes> 
         <Route path="/" element={<Home/>}/>
         <Route path="/popular" element={<Popular/>}/>
       </Routes>
     </BrowserRouter>
   )
 }
+
+
 
 /*gonna add create-geddit route as well as post route */
 
