ade441a88f416e7eae84ef082f7e577259819fca
lernEarnBurn <lerners124@gmail.com>
2023-05-21 22:52:40 +0300

    got images working on posts

--- src/components/contentScreen.tsx
+++ src/components/contentScreen.tsx
@@ -16,7 +16,7 @@ export function ContentScreen() {
             </div>
           </div>
           <Post 
-            content="https://lh3.googleusercontent.com/a/AGNmyxasMJkHoX7NA_RAoSZa-sKgDkPgYl0HP6xscDqn=s96-c" 
+            content="https://images.unsplash.com/photo-1517423440428-a5a00ad493e8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=449&q=80" 
             title="making props to work for posts" 
             user="lerner1737@gmail.com" 
             subgeddit="AmITheAsshole" 
--- src/components/post.tsx
+++ src/components/post.tsx
@@ -29,7 +29,7 @@ export function Post(props: PostProps){
         <div className="text-gray-100 w-[40vw] mt-2 bg-gray-800 rounded-sm px-3 py-2  hover:border hover:border-gray-50 hover:border-1 flex">
               <div className="mr-4 flex-col mt-1">
                 <div className="w-[1.2vw] h-[3vh] bg-cover up"></div>
-                <div className="text-sm ml-1">{props.upvotes}</div>
+                <div className="text-sm ml-[.2vw]">{props.upvotes}</div>
                 <div className="w-[1.2vw] h-[3vh] bg-cover down"></div>
               </div>
               <div className="flex-col gap-6 justify-center">
@@ -37,13 +37,14 @@ export function Post(props: PostProps){
                   <div className="text-sm cursor-pointer hover:underline">g/{props.subgeddit}</div>
                   <div className="text-xs">Posted by <span className="cursor-pointer hover:underline">u/{props.user}</span> {props.timeStamp}</div>
                 </div>
-                <div className="text-xl">{props.title}</div>
+                <div className="text-xl mb-2">{props.title}</div>
                 {!hasImage ? (
                     <div className="text-sm">
                         {props.content}
                     </div>
                 ) : (
-                    <div className="bg-contain w-[100%] h-[20vw]" style={{ backgroundImage: `url(${props.content})`, backgroundSize: '100% 100%' }}></div>
+                    /*figure out how to properly size images and possibly figure out aspect ratios */
+                    <div className="object-cover bg-center bg-no-repeat bg-cover w-[36vw] h-[40vh]" style={{ backgroundImage: `url(${props.content})`}}></div>
 
                 )}
                 
