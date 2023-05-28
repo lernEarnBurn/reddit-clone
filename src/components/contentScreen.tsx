171747dd2ac91c5da8f3004e5409362770f22e0d
lernEarnBurn <lerners124@gmail.com>
2023-05-28 09:46:00 +0300

    made a little change to style

--- src/components/contentScreen.tsx
+++ src/components/contentScreen.tsx
@@ -3,9 +3,8 @@ import { Post } from './post';
 
 export function ContentScreen() {
   return (
-    <>
-      <div className="bg-gray-700 h-auto min-h-[91vh] w-screen absolute flex justify-center">
-        <div>
+    <div className="flex justify-center">
+        <div className="flex-col">
           <div className="w-[40vw] h-[8vh] bg-gray-800 flex items-center justify-evenly mt-2 rounded-sm ">
             <div className="flex primary-foreground">
               <img src="/images/add.svg"></img>
@@ -25,6 +24,14 @@ export function ContentScreen() {
             upvotes={88}
           />
           <Post
+            content="https://images.unsplash.com/photo-1517423440428-a5a00ad493e8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=449&q=80"
+            title="making props to work for posts"
+            user="lerner1737@gmail.com"
+            subgeddit="AmITheAsshole"
+            timeStamp="3 days ago"
+            upvotes={88}
+          />
+          <Post
             content="://images.unsplash.com/photo-1517423440428-a5a00ad4 93e8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by 1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=449&q=80"
             title="making props to work for posts"
             user="lerner1737@gmail.com"
@@ -34,7 +41,6 @@ export function ContentScreen() {
           />
         </div>
         <PageInfo subgeddit='AmITheAssholes'/>
-      </div>
-    </>
+    </div>
   );
 }
--- src/components/pageInfo.tsx
+++ src/components/pageInfo.tsx
@@ -6,12 +6,12 @@ interface PageInfoProps {
 
 export function PageInfo(props: PageInfoProps) {
   return (
-    <div className="ml-2 mt-2 max-w-[20vw] max-h-[50vh] rounded-sm bg-gray-800 flex-col justify-end">
+    <div className="ml-2 mt-2 max-w-[20vw] rounded-sm bg-gray-800 flex-col justify-end">
       <div
         className="primary-foreground mt-6 text-2xl font-bold flex justify-center">
         <p>g/{props.subgeddit}</p>
       </div>
-      <div className="primary-foreground px-6 py-4">This is the dasddddddd dddddass ssssss sssssssss ssssssss sssescription and it is goi to be long as hell. This is the description and it is goi to be long as hell</div>
+      <div className="primary-foreground px-6 py-4">This is the dasddddddd dddddass ssssss sssssssss ssssssss ssses This is the dasddddddd dddddass ssssss sssssssss ssssssss ssses cription and it is goi to be long as hell. This is the description and it is goi to be long as hell</div>
       <div className="flex gap-14 justify-center">
         <div className="text-xs text-center text-gray-100"><div className="text-2xl">78</div>Followers</div>
         <div className="text-xs text-center text-gray-100"><div className="text-2xl">30</div>Online RN</div>
