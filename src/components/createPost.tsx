20348f16beff5e10ca4343d1e5b786650d210732
lernEarnBurn <lerners124@gmail.com>
2023-05-29 22:26:22 +0300

    added subgeddit input field

--- src/components/createPost.tsx
+++ src/components/createPost.tsx
@@ -28,7 +28,6 @@ export function CreatePost() {
           setCurrentImage(`url(${e.target?.result})`)
         }
         reader.readAsDataURL(file);
-      
     }
   }
 
@@ -39,16 +38,19 @@ export function CreatePost() {
                 <button onClick={changePostType} className={`primary-foreground w-[50%] text-2xl hover:opacity-90 flex justify-center items-center ${onTextMode && 'selected'}`}>Text</button>
                 <button onClick={changePostType} className={`primary-foreground w-[50%] text-2xl hover:opacity-90 flex justify-center items-center ${!onTextMode && 'selected'}`}>Image</button>
               </div>
+              /*When focus on subgeddit input make a list of the users followed subgeddits appear */
               {onTextMode ? (
                 <>
-                  <Input className="mt-6 text-gray-200  border-gray-900 focus:border-gray-200 text-md h-[7vh]" placeholder='Title'/>
-                  <Textarea className=' text-gray-200 border-gray-900 focus:border-gray-200 max-h-[58vh] min-h-[58vh] text-md' placeholder='Text(Optional)'/>
+                  <Input className="mt-6 text-gray-200  border-gray-900 focus:border-gray-200 h-[7vh] text-md" placeholder='Subgeddit'/>                
+                  <Input className=" text-gray-200  border-gray-900 focus:border-gray-200 text-md h-[7vh]" placeholder='Title'/>
+                  <Textarea className=' text-gray-200 border-gray-900 focus:border-gray-200 max-h-[52vh] min-h-[52vh] text-md' placeholder='Text(Optional)'/>
                   <Button className='mt-4 w-[42vw] h-[6vh] text-xl ml-6'>Post</Button>
                 </>
               ) : (
                 <>
-                  <Input className="mt-6 text-gray-200  border-gray-900 focus:border-gray-200 h-[7vh] text-md" placeholder='Title'/>
-                  <button style={{backgroundImage: currentImage}} onClick={() => {imageUploaderRef.current?.click();}} className={`w-full h-[58vh] border border-gray-900 focus:border-gray-200 rounded-sm text-lg text-gray-500 bg-center bg-no-repeat bg-cover ${currentImage === "" ? "text-opacity-100" : "text-opacity-0"}`}>Click To Upload Image</button>
+                  <Input className="mt-6 text-gray-200  border-gray-900 focus:border-gray-200 h-[7vh] text-md" placeholder='Subgeddit'/>                
+                  <Input className=" text-gray-200  border-gray-900 focus:border-gray-200 h-[7vh] text-md" placeholder='Title'/>
+                  <button style={{backgroundImage: currentImage}} onClick={() => {imageUploaderRef.current?.click();}} className={`w-full h-[52vh] border border-gray-900 focus:border-gray-200 rounded-sm text-lg text-gray-500 bg-center bg-no-repeat bg-cover ${currentImage === "" ? "text-opacity-100" : "text-opacity-0"}`}>Click To Upload Image</button>
                   <input onChange={displaySelectedImage} ref={imageUploaderRef} className='hidden' type="file" accept="image/*" />
                   <Button className='mt-4 w-[42vw] h-[6vh] text-xl ml-6'>Post</Button>
 
