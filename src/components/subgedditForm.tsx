4252516859637c39a1e736e252107ebfacb36775
lernEarnBurn <lerners124@gmail.com>
2023-05-30 23:39:38 +0300

    started implementing backend

--- src/components/subgedditForm.tsx
+++ src/components/subgedditForm.tsx
@@ -2,11 +2,35 @@ import { Input } from './ui/input';
 import { Button } from './ui/button';
 import { Textarea } from './ui/textarea';
 
+import { useRef } from 'react';
+
+import { addDoc, collection, getFirestore } from 'firebase/firestore'
+
 interface SubgedditFormProps {
   setCreateSubgeddit: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export function SubgedditForm(props: SubgedditFormProps) {
+
+  const nameRef = useRef<HTMLInputElement>(null)
+  const descriptRef = useRef<HTMLTextAreaElement>(null)
+
+
+  async function createCommunity() {  
+    try {
+      await addDoc(collection(getFirestore(), "subgeddits"), {
+        name: nameRef.current?.value,
+        description: descriptRef.current?.value,
+        leader: localStorage.getItem('user'),
+        followers: 0,
+        posts: []
+      });
+    } catch (error) {
+      console.error("Error writing new message to Firebase Database", error);
+    }
+    props.setCreateSubgeddit(false)
+  }
+
   return (
     <>
       <div className="fixed left-0 top-0 z-10 flex h-full w-full items-center justify-center bg-gray-500 bg-opacity-50">
@@ -23,12 +47,13 @@ export function SubgedditForm(props: SubgedditFormProps) {
             </button>
           </div>
           <div className="flex-col">
-            <Input className="mb-2 mt-14" placeholder="Name"></Input>
+            <Input ref={nameRef} className="mb-2 mt-14" placeholder="Name"></Input>
             <Textarea
+              ref={descriptRef}
               className="mb-2 max-h-[35vh] min-h-[20vh]"
               placeholder="Description"
             />
-            <Button className="w-full">Create</Button>
+            <Button onClick={createCommunity} className="w-full">Create</Button>
           </div>
         </div>
       </div>
