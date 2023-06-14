12e133b0d5ed31daf71ea248a0cffcfb141d0162
lernEarnBurn <lerners124@gmail.com>
2023-06-14 22:56:30 +0300

    function to get post details

--- src/components/postPage.tsx
+++ src/components/postPage.tsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-export  function PostPage() {
+import { getFirestore, DocumentData, doc, DocumentReference, getDoc } from "firebase/firestore";
+
+export function PostPage() {
+    const {id} = useParams()
+
+    const [post, setPost] = useState<DocumentData>({})
+
+    useEffect(() => {
+        async function getPostDetails(): Promise<void>{
+            
+            const db = getFirestore()
+            if(id){
+                const postRef: DocumentReference = doc(db, 'posts', id);
+                
+                const postSnapshot = await getDoc(postRef);
+                if (postSnapshot.exists()) {
+                  const postData = postSnapshot.data();
+                  setPost(postData)  
+                }
+            }
+        }
+        
+        getPostDetails()
+        
+    }, [id])
+
     return (
-        <div className="text-green-500">Hello</div>
+        <div className="text-green-500">{JSON.stringify(post)}</div>
     )
 }
